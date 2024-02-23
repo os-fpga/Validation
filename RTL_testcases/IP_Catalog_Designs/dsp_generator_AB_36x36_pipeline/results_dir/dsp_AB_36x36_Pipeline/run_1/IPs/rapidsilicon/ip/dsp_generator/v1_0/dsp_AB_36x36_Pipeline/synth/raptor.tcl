2ac9ede04198b8c3fde4d46009b281425e26a317
Sarmad-Salman <sarmadsalman30@gmail.com>
2024-02-23 16:00:05 +0500

    removed results_dir

--- RTL_testcases/IP_Catalog_Designs/dsp_generator_AB_36x36_pipeline/results_dir/dsp_AB_36x36_Pipeline/run_1/IPs/rapidsilicon/ip/dsp_generator/v1_0/dsp_AB_36x36_Pipeline/synth/raptor.tcl
+++
@@ -1,8 +0,0 @@
-create_design dsp_AB_36x36_Pipeline
-target_device GEMINI
-add_include_path ../src
-add_library_path ../src
-add_library_ext .v .sv
-add_design_file ../src/dsp_AB_36x36_Pipeline_v1_0.v
-set_top_module dsp_AB_36x36_Pipeline
-synthesize
