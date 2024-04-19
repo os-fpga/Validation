04c06b6e0d1ec835e2c8a003c5f2d3ef66b1e80b
abdulhameed-akram <aahameedd@gmail.com>
2024-04-19 17:24:46 +0500

    pll tb update

--- RTL_testcases/RS_FPGA_PRIMITIVES_new/PLL_primitive_inst/sim/co_sim_tb/co_sim_PLL_primitive_inst.v
+++ RTL_testcases/RS_FPGA_PRIMITIVES_new/PLL_primitive_inst/sim/co_sim_tb/co_sim_PLL_primitive_inst.v
@@ -26,4 +26,50 @@ PLL_primitive_inst	golden (.*);
     end
 // Initialize values to zero 
 initial	begin
-	
+	{PLL_EN} <= 'd0;
+	reset <= 1 ;
+	 repeat (2) @ (negedge CLK_IN); 
+	compare();
+	//Random stimulus generation
+	repeat(100) @ (negedge CLK_IN) begin
+		PLL_EN <= 1;
+		reset <= 0;
+
+		compare();
+	end
+	//Random stimulus generation
+	repeat(100) @ (negedge CLK_IN) begin
+		PLL_EN <= $random();
+		reset <= $random();
+
+		compare();
+	end
+
+	// ----------- Corner Case stimulus generation -----------
+	PLL_EN <= 1;
+	reset <= 1;
+	repeat (2) @ (negedge CLK_IN);
+	compare();
+	if(mismatch == 0)
+		$display("**** All Comparison Matched *** \n		Simulation Passed\n");
+	else
+		$display("%0d comparison(s) mismatched\nERROR: SIM: Simulation Failed", mismatch);
+	#50;
+	$finish;
+end
+
+task compare();
+	if ( CLK_OUT !== CLK_OUT_netlist	||	CLK_OUT_DIV2 !== CLK_OUT_DIV2_netlist	||	CLK_OUT_DIV3 !== CLK_OUT_DIV3_netlist	||	CLK_OUT_DIV4 !== CLK_OUT_DIV4_netlist	||	LOCK !== LOCK_netlist	||	SERDES_FAST_CLK !== SERDES_FAST_CLK_netlist ) begin
+		$display("Data Mismatch: Actual output: %0d, %0d, %0d, %0d, %0d, %0d, Netlist Output %0d, %0d, %0d, %0d, %0d, %0d, Time: %0t ", CLK_OUT, CLK_OUT_DIV2, CLK_OUT_DIV3, CLK_OUT_DIV4, LOCK, SERDES_FAST_CLK, CLK_OUT_netlist, CLK_OUT_DIV2_netlist, CLK_OUT_DIV3_netlist, CLK_OUT_DIV4_netlist, LOCK_netlist, SERDES_FAST_CLK_netlist,  $time);
+		mismatch = mismatch+1;
+	end
+	else
+		$display("Data Matched: Actual output: %0d, %0d, %0d, %0d, %0d, %0d, Netlist Output %0d, %0d, %0d, %0d, %0d, %0d, Time: %0t ", CLK_OUT, CLK_OUT_DIV2, CLK_OUT_DIV3, CLK_OUT_DIV4, LOCK, SERDES_FAST_CLK, CLK_OUT_netlist, CLK_OUT_DIV2_netlist, CLK_OUT_DIV3_netlist, CLK_OUT_DIV4_netlist, LOCK_netlist, SERDES_FAST_CLK_netlist,  $time);
+endtask
+
+initial begin
+	$dumpfile("tb.vcd");
+	$dumpvars;
+end
+
+endmodule
