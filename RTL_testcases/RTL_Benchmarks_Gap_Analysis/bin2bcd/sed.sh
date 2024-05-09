7b202bc21b45830c6594c8ffe4ed18189850f68b
zhergarvi <zhergarvi133@gmail.com>
2024-05-09 16:16:12 +0500

    updated the bin2bcd bitstream sim testcase

--- RTL_testcases/RTL_Benchmarks_Gap_Analysis/bin2bcd/sed.sh
+++ RTL_testcases/RTL_Benchmarks_Gap_Analysis/bin2bcd/sed.sh
@@ -1,6 +1,6 @@
 
-sed -i "s|// 	wire \[0:0\] bcd\[0\]_gfpga;|   integer i;|" $(find . -type f -name "fabric_bin2bcd_formal_random_top_tb.v")
-sed -i "s|// 	wire \[0:0\] bcd\[1\]_gfpga;|   integer mismatch=0;|" $(find . -type f -name "fabric_bin2bcd_formal_random_top_tb.v")
+# sed -i "s|// 	wire \[0:0\] bcd\[1\]_gfpga;|    integer i;|" $(find . -type f -name "fabric_bin2bcd_formal_random_top_tb.v")
+sed -i "s|// 	wire \[0:0\] bcd\[2\]_gfpga;|    integer mismatch=0;|" $(find . -type f -name "fabric_bin2bcd_formal_random_top_tb.v")
 # sed -i "s|// 	wire \[0:0\] out\[1\]_gfpga;|    reg \[4:0\] counter_model=5'b00000;|" $(find . -type f -name "fabric_bin2bcd_formal_random_top_tb.v")
 # sed -i "s|dumpvars(1|dumpvars(0|" $(find . -type f -name "fabric_bin2bcd_formal_random_top_tb.v")
 # sed -i "s|clk_fm\[15\] = 1'b0|clk_fm\[15\] = clock0|" $(find . -type f -name "fabric_bin2bcd_top_formal_verification.v")
