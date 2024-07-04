55012bfd22143ffe2cc8dca9070be600a07bdd9c
Abdul Hameed <45313849+abdulhameed-akram@users.noreply.github.com>
2024-07-04 12:51:30 +0500

    Fix for FR_daily fails (#244)

--- RTL_testcases/RS_Primitive_example_designs/primitive_example_design_5/rtl/primitive_example_design_5.v
+++ RTL_testcases/RS_Primitive_example_designs/primitive_example_design_5/rtl/primitive_example_design_5.v
@@ -17,7 +17,7 @@ module primitive_example_design_5(
     wire i_clk_buf_out;
 
 
-    I_BUF i_buf_inst (.I(clk),.EN(ibuf_oe1),.O(i_clk_buf_out));
+    I_BUF i_buf_inst (.I(clk),.EN(1),.O(i_clk_buf_out));
     CLK_BUF clk_buf_inst (.I(i_clk_buf_out), .O(clk_buf_out));
    
     I_DDR iddr_ist1 (.D(i_buf_out[0]),.R(rst_i_buf_out),.E(iddr_en[0]),.C(clk_buf_out),.Q(iddr_out[0]));
