5f98537a3efc004bac4d7c6da0241a1e46bf93f3
alaindargelas <alain.dargelas@gmail.com>
2024-07-04 09:54:14 -0700

    GJC4 requires 1VG28 for DSP ressources

--- RTL_testcases/GJC-IO-Testcases/GJC4/raptor_run.sh
+++ RTL_testcases/GJC-IO-Testcases/GJC4/raptor_run.sh
@@ -15,7 +15,7 @@ post_route_sim=true
 bitstream_sim=false
 
 #raptor options
-device="GEMINI_COMPACT_10x8"
+device="1VG28"
 
 strategy="delay" #(area, delay, mixed, none) 
 
@@ -170,7 +170,7 @@ parse_cga exit 1; }
     cd ..
     
     echo "create_design $design">raptor_tcl.tcl 
-    echo "target_device GEMINI_COMPACT_10x8">>raptor_tcl.tcl 
+    echo "target_device 1VG28">>raptor_tcl.tcl 
 
     ##vary design to design
     [ -z "$ip_name" ] && echo "" || echo "configure_ip $ip_name"_v1_0" -mod_name=$design -Pdata_width=32 -Paddr_width=16 -Pid_width=32 -Pa_pip_out=0 -Pb_pip_out=0 -Pa_interleave=0 -Pb_interleave=0 -out_file ./$design.v">>raptor_tcl.tcl
