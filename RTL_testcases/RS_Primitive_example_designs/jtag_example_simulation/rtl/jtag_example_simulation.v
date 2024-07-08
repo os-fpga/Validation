a3ab097ce6d290f8876a56c74fa73d7853c778f6
zhergarvi <91175124+zhergarvi@users.noreply.github.com>
2024-07-08 14:34:24 +0500

    added register in rtl of jtag_example_simulation testcase (#251)

--- RTL_testcases/RS_Primitive_example_designs/jtag_example_simulation/rtl/jtag_example_simulation.v
+++ RTL_testcases/RS_Primitive_example_designs/jtag_example_simulation/rtl/jtag_example_simulation.v
@@ -6,6 +6,8 @@ module jtag_example_simulation (
   output reset_n,
   input en
 );
+
+  wire enable;
   
   SOC_FPGA_INTF_JTAG inst (
     .BOOT_JTAG_TCK(clock),
@@ -13,7 +15,14 @@ module jtag_example_simulation (
     .BOOT_JTAG_TDO(tdo),
     .BOOT_JTAG_TMS(tms),
     .BOOT_JTAG_TRSTN(reset_n),
-    .BOOT_JTAG_EN(en)
+    .BOOT_JTAG_EN(enable)
   );
+
+  always @(posedge clock) begin
+    if (!reset_n)
+      enable <= 0;
+    else
+      enable <= en;
+  end
 
 endmodule
