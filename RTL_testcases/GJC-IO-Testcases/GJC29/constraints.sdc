8adb9a6f457f222e308a3476c14592affdb158f1
zhergarvi <zhergarvi133@gmail.com>
2024-07-09 11:56:05 +0500

    changed device for north south west east to 22x4

--- RTL_testcases/GJC-IO-Testcases/GJC29/constraints.sdc
+++ RTL_testcases/GJC-IO-Testcases/GJC29/constraints.sdc
@@ -1,6 +1,4 @@
-set_top_module GJC29
-
-create_clock -period 10 -name clk_i_buf
+create_clock -period 10 clk_i_buf
 
 set_output_delay 3 -clock clk_i_buf [get_ports {dly_tap_val_inv_buf}]
 
--- RTL_testcases/GJC-IO-Testcases/GJC3south/raptor_run.sh
+++ RTL_testcases/GJC-IO-Testcases/GJC3south/raptor_run.sh
@@ -15,7 +15,7 @@ post_route_sim=true
 bitstream_sim=false
 
 #raptor options
-device="GEMINI_COMPACT_104x68"
+device="GEMINI_COMPACT_22x4"
 
 strategy="delay" #(area, delay, mixed, none) 
 
@@ -170,7 +170,7 @@ parse_cga exit 1; }
     cd ..
     
     echo "create_design $design">raptor_tcl.tcl 
-    echo "target_device 1VG28">>raptor_tcl.tcl 
+    echo "target_device GEMINI_COMPACT_22x4">>raptor_tcl.tcl 
 
     ##vary design to design
     [ -z "$ip_name" ] && echo "" || echo "configure_ip $ip_name"_v1_0" -mod_name=$design -Pdata_width=32 -Paddr_width=16 -Pid_width=32 -Pa_pip_out=0 -Pb_pip_out=0 -Pa_interleave=0 -Pb_interleave=0 -out_file ./$design.v">>raptor_tcl.tcl
@@ -192,7 +192,7 @@ parse_cga exit 1; }
 
     ##vary design to design
     [ -z "$add_constraint_file" ] && echo "" || echo "add_constraint_file $add_constraint_file">>raptor_tcl.tcl 
-    
+    echo "add_constraint_file pin_constraints.pin">>raptor_tcl.tcl 
 
 	echo "analyze">>raptor_tcl.tcl
     [ -z "$verific_parser" ] && echo "" || echo "verific_parser $verific_parser">>raptor_tcl.tcl
