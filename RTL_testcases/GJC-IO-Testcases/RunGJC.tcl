a22621019adbf9777c90a875943c5554caaa0a60
alaindargelas <alain.dargelas@gmail.com>
2024-07-10 14:02:21 -0700

    Add log file

--- RTL_testcases/GJC-IO-Testcases/RunGJC.tcl
+++ RTL_testcases/GJC-IO-Testcases/RunGJC.tcl
@@ -7,12 +7,22 @@ set raptor_path "/home/alain/os-fpga/Raptor/build/bin/raptor"
 
 regexp {raptor_path=([a-zA-Z0-9/\.-]+)} $argv tmp raptor_path
 
-puts "Raptor: $raptor_path"
-puts "GJC Begin Run"
+set logId [open "RunGJC.log" "w"]
+
+proc log { msg } {
+    global logId
+    puts $msg
+    puts $logId $msg
+    flush $logId
+}
+
+
+log "Raptor: $raptor_path"
+log "GJC Begin Run"
 
 foreach dirName [glob -nocomplain -type {d} GJC* ] {
     cd $dirName
-    puts "Creating raptor_tcl.tcl for TESTCASE: $dirName"
+    log "Creating raptor_tcl.tcl for TESTCASE: $dirName"
     set result ""
     exec sh -c "rm -rf results_dir"
     catch {set result [exec sh -c "./raptor_run.sh"]} result
@@ -24,14 +34,13 @@ foreach dirName [glob -nocomplain -type {d} GJC* ] {
 
 foreach dirName [glob -nocomplain -type {d} GJC* ] {
     cd $dirName/results_dir
-    puts "Running TESTCASE: $dirName"
+    log "Running TESTCASE: $dirName"
     set result ""
     catch {set result [exec sh -c "$raptor_path --script ../raptor_tcl.tcl --batch"]} result
     if ![regexp "bitstream is generated" $result] {
-        puts "  Test failed: $dirName/results_dir/raptor.log"
-        puts "        Error: [string range $result 0 200]"
+        log "  Test failed: $dirName/results_dir/raptor.log"
     }
     cd ../..
 }
 
-puts "GJC End Run"
+log "GJC End Run"
