aac520df10ae8129d0d6901953f23baa03dabab1
zhergarvi <91175124+zhergarvi@users.noreply.github.com>
2024-07-15 12:28:32 +0500

    updated testbench of BOOT_CLOCK_primitive_inst (#265)

--- RTL_testcases/RS_FPGA_PRIMITIVES_new/BOOT_CLOCK_primitive_inst/sim/co_sim_tb/co_sim_BOOT_CLOCK_primitive_inst.v
+++ RTL_testcases/RS_FPGA_PRIMITIVES_new/BOOT_CLOCK_primitive_inst/sim/co_sim_tb/co_sim_BOOT_CLOCK_primitive_inst.v
@@ -1,7 +1,8 @@
 `timescale 1ns/1ps
 module co_sim_BOOT_CLOCK_primitive_inst;
 // Clock signals
-    reg clk1;
+    reg			clk1;
+    wire 		\$fclk_buf_O_BOOT_CLOCK ;
     wire 		O_BOOT_CLOCK	,	O_BOOT_CLOCK_netlist;
     reg 		async_signal;
     wire 		sync_signal	,	sync_signal_netlist;
@@ -15,34 +16,35 @@ BOOT_CLOCK_primitive_inst	golden (.*);
 	BOOT_CLOCK_primitive_inst_post_synth synth_net (.*, .O_BOOT_CLOCK(O_BOOT_CLOCK_netlist), .sync_signal(sync_signal_netlist) );
 `endif
 
-//clock initialization for clk1
-    initial begin
-        clk1 = 1'b0;
-        forever #5 clk1 = ~clk1;
-    end
+// clock initialization for clk1
+initial begin
+	clk1 = 1'b0;
+	forever #1 clk1 = ~clk1;
+end
+
 // Initialize values to zero 
 initial	begin
-	repeat (2) @ (negedge clk1);
+	// repeat (2) @ (negedge clk1);
 
-	async_signal <= 'd0;
-	 repeat (2) @ (negedge clk1); 
+	async_signal <= 'd1;
+	 repeat (10) @ (negedge clk1); 
 	compare();
 	//Random stimulus generation
-	repeat(100) @ (negedge clk1) begin
-		async_signal <= $random();
+	repeat(1000) @ (negedge clk1) begin
+		async_signal         <= $urandom();
 
 		compare();
 	end
 
 	// ----------- Corner Case stimulus generation -----------
-	async_signal <= 1;
+	async_signal           <= 0;
 	repeat (2) @ (negedge clk1);
 	compare();
 	if(mismatch == 0)
 		$display("**** All Comparison Matched *** \n		Simulation Passed\n");
 	else
 		$display("%0d comparison(s) mismatched\nERROR: SIM: Simulation Failed", mismatch);
-	#50;
+	#20;
 	$finish;
 end
 
