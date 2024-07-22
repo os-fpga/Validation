a442e3b264f4717c408470d18f06ae32fb648814
Abdul Hameed <45313849+abdulhameed-akram@users.noreply.github.com>
2024-07-22 15:03:30 +0500

    Update mkDelayWorker32B.v

--- RTL_testcases/Benchmarking_QoR_testcases/mkDelayWorker32B/rtl/mkDelayWorker32B.v
+++ RTL_testcases/Benchmarking_QoR_testcases/mkDelayWorker32B/rtl/mkDelayWorker32B.v
@@ -6531,10 +6531,10 @@ output	[1:0]		level;
 // Local Wires
 //
  
-reg	[`awx-1:0]	wp;
+reg	[`awx-1:0]	wp=0;
 wire	[`awx-1:0]	wp_pl1;
 wire	[`awx-1:0]	wp_pl2;
-reg	[`awx-1:0]	rp;
+reg	[`awx-1:0]	rp=0;
 wire	[`awx-1:0]	rp_pl1;
 reg			full_r;
 reg			empty_r;
