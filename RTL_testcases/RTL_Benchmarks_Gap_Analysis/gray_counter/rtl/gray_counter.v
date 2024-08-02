c19dba29365965d721fb958495995ef0455930bf
zhergarvi <zhergarvi133@gmail.com>
2024-08-02 18:09:59 +0500

    updated gray_counter design

--- RTL_testcases/RTL_Benchmarks_Gap_Analysis/gray_counter/rtl/gray_counter.v
+++ RTL_testcases/RTL_Benchmarks_Gap_Analysis/gray_counter/rtl/gray_counter.v
@@ -4,25 +4,30 @@
 //-------------------------------------------------------
 
 `timescale 1ns / 1ps
-`ifndef TESTBENCH
-`define size 5
-`endif
 
-module gray_counter
- 
-  (	input 	clk,
-	input 	rst,
-	output reg [`size-1:0] out);
-  
-	reg [`size-1:0] q; 	 	
-  
-	always @ (posedge clk) begin
-		if (rst) begin
-    		q <= 0;
-    		out <= 0;
-      	end else begin
-  			q <= q + 1;
-			out <= {q[`size-1], q[`size-1:1] ^ q[`size-2:0]};
-     	end
- 	end
+module gray_counter (
+    input clk,
+    input rst,
+    output reg [5:0] out
+);
+
+reg [5:0] q; 
+
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        q <= 6'b000000;
+    end else begin
+        q <= q + 1;
+    end
+end
+
+always @(*) begin
+    out[5] = q[5];
+    out[4] = q[5] ^ q[4];
+    out[3] = q[4] ^ q[3];
+    out[2] = q[3] ^ q[2];
+    out[1] = q[2] ^ q[1];
+    out[0] = q[1] ^ q[0];
+end
+
 endmodule
