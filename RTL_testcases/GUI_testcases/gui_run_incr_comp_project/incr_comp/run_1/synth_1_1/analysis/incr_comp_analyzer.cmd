0532a3f5518d04895f942697d09d9619bad55cac
zhergarvi <91175124+zhergarvi@users.noreply.github.com>
2024-09-18 18:31:24 +0500

    added incr_comp raptor example (#360)

--- RTL_testcases/GUI_testcases/gui_run_incr_comp_project/incr_comp/run_1/synth_1_1/analysis/incr_comp_analyzer.cmd
+++
@@ -1,5 +0,0 @@
-read_verilog -sv /nfs_eda_sw/softwares/Raptor/instl_dir/07_24_2024_09_15_01/share/raptor/sim_models/rapidsilicon/genesis3/FPGA_PRIMITIVES_MODELS/blackbox_models/cell_sim_blackbox.v
-verilog_defines 
-read_verilog  -I../../../../.. -I/nfs_scratch/scratch/CompilerValidation/abdul_hameed/24july/Validation/RTL_testcases/GUI_testcases/gui_run_incr_comp_project/incr_comp/run_1/incr_comp.srcs/sources_1  /nfs_scratch/scratch/CompilerValidation/abdul_hameed/24july/Validation/RTL_testcases/GUI_testcases/gui_run_incr_comp_project/incr_comp/run_1/incr_comp.srcs/sources_1/tmp.v 
-
-analyze -top and2
